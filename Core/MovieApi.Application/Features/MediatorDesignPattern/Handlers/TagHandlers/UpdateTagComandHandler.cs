50504a0a6dc38c35535e95e0c24d4824662937b1
İsmail Baran <ismailbaran04@gmail.com>
2025-04-08 10:22:58 +0300

    cqrs ve mediatr tasarım desenleri kodlanıyor

---
+++ Core/MovieApi.Application/Features/MediatorDesignPattern/Handlers/TagHandlers/UpdateTagComandHandler.cs
@@ -0,0 +1,23 @@
+﻿using MediatR;
+using MovieApi.Application.Features.MediatorDesignPattern.Command.TagCommands;
+using MovieApi.Persistence.Context;
+
+namespace MovieApi.Application.Features.MediatorDesignPattern.Handlers.TagHandlers
+{
+    public class UpdateTagComandHandler:IRequestHandler<UpdateTagCommand>
+    {
+        private readonly MovieContext _context;
+
+        public UpdateTagComandHandler(MovieContext context)
+        {
+            _context = context;
+        }
+
+        public async Task Handle(UpdateTagCommand request, CancellationToken cancellationToken)
+        {
+            var values = await _context.Tags.FindAsync(request.TagId);
+            values.Title = request.Title;
+            await _context.SaveChangesAsync();
+        }
+    }
+}
---
+++ Core/MovieApi.Application/Features/MediatorDesignPattern/Queries/TagQueries/GetTagByIdQuery.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using MovieApi.Application.Features.MediatorDesignPattern.Results.TagResults;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieApi.Application.Features.MediatorDesignPattern.Queries.TagQueries
+{
+    public class GetTagByIdQuery : IRequest<GetTagByIdQueryResult>
+    {
+        public GetTagByIdQuery(int tagId)
+        {
+            TagId = tagId;
+        }
+
+        public int TagId { get; set; }
+        
+    }
+}
