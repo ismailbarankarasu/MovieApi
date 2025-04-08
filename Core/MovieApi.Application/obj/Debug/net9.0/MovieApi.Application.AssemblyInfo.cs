c7c22952ec11f5b76d1b7f2b54b485468619f0c1
İsmail Baran <ismailbaran04@gmail.com>
2025-04-08 14:28:10 +0300

    otomatik güncellemeler işlendi

--- Core/MovieApi.Application/obj/Debug/net9.0/MovieApi.Application.AssemblyInfo.cs
+++ Core/MovieApi.Application/obj/Debug/net9.0/MovieApi.Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieApi.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05cdc40312d1fd16ca00ea42b1f082ba0dda2356")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd601dda79010d31447ed2b150ac682b8a3c40dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieApi.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieApi.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
