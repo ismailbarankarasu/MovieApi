7b9a7c3257995a857cf6911b1cfc9f278d4dbc6e
İsmail Baran <ismailbaran04@gmail.com>
2025-04-08 10:22:25 +0300

    otomatik güncellemeler işlendi

--- Infrastructure/MovieApi.Persistence/obj/Debug/net9.0/MovieApi.Persistence.AssemblyInfo.cs
+++ Infrastructure/MovieApi.Persistence/obj/Debug/net9.0/MovieApi.Persistence.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MovieApi.Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+05cdc40312d1fd16ca00ea42b1f082ba0dda2356")]
 [assembly: System.Reflection.AssemblyProductAttribute("MovieApi.Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MovieApi.Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
